103de4580a1453a38bac74b16168bb57069f03ae
Yate Ge (葛亚特) <geyate@gmail.com>
2023-07-05 11:36:57 +0800

    实现 js2temi

--- GPT2Temi/userscript.js
+++ GPT2Temi/userscript.js
@@ -1,19 +1,25 @@
-// async function main() {
-//   try {
-//     await robot.goto("葛亚特的座位");
-//     await robot.wait(5000); // 等待5秒，看看葛亚特是否在座位上
-//     const detectResult = await robot.detectHuman();
-//     if (detectResult) {
-//       await robot.speak("葛亚特在座位上。");
-//     } else {
-//       await robot.speak("很抱歉，葛亚特不在座位上。");
-//     }
-//     await robot.goto("客厅");
-//     await robot.speak("经过查询，葛亚特" + (detectResult ? "在" : "不在") + "座位上。");
-//   } catch (error) {
-//     console.log(error);
-//     await robot.goto("充电桩"); // 出现错误时，前往充电桩
-//   }
-// }
+const members = ['小明', '小绿', '小白'];
+const availableMembers = [];
+let reply = '';
 
-// main();
+await robot.userRequest('预约开组会');
+
+for (let i = 0; i < members.length; i++) {
+  await robot.goto(members[i] + '的座位');
+  const isPresent = await robot.detectHuman();
+  if (isPresent) {
+    const isAvailable = await robot.ask('您好，' + members[i] + '，您现在有时间参加组会吗？');
+    if (isAvailable === '有的') {
+      availableMembers.push(members[i]);
+    }
+  }
+}
+
+if (availableMembers.length > 0) {
+  reply = '以下成员有时间参加组会：' + availableMembers.join('、');
+} else {
+  reply = '很遗憾，目前没有成员有时间参加组会。';
+}
+
+await robot.goto('客厅');
+await robot.speak(reply);
